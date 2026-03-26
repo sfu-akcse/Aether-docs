e86e2add72aeb079d736fa4b19e8baf0c68b07f8
kwanghyukryu <kwanisop@gmail.com>
2026-03-25 19:27:28 -0700

    Issue3/python-opencv-mediapipe documentation

--- sidebars.js
+++ sidebars.js
@@ -22,6 +22,7 @@ const sidebars = {
 
   softwareTeamSidebar: [
     "software-team/overview",
+    "software-team/socket-communication",
     "software-team/python-opencv-mediapipe",
     {
       type: "category",
